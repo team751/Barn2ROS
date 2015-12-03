a6afac2c5b5b1daa3370693d3439ad4d7971b5fd
Sam Baumgarten <sambaumgarten98@gmail.com>
2015-12-03 13:37:02 -0800

    change motor_sender.py node name

--- src/roborio_comms/src/motor_sender.py
+++ src/roborio_comms/src/motor_sender.py
@@ -34,13 +34,7 @@ def callback(data):
     output[controllers.index(data._connection_header["topic"])] = data.data
     
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('motor_sender', anonymous=True)
 
     for controller in controllers:
         rospy.Subscriber(controller, Float32, callback)
