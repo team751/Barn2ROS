85b0cd8950353dc792169a53ef8eaec9bb2abeeb
ROS - TK1 (1) <sbaumgarten16@priorypanther.com>
2016-02-15 19:44:30 +0000

    Configure for arm

---
+++ devel/share/common-lisp/ros/teb_local_planner/msg/_package.lisp
@@ -0,0 +1,13 @@
+(cl:defpackage teb_local_planner-msg
+  (:use )
+  (:export
+   "<TRAJECTORYMSG>"
+   "TRAJECTORYMSG"
+   "<FEEDBACKMSG>"
+   "FEEDBACKMSG"
+   "<OBSTACLEMSG>"
+   "OBSTACLEMSG"
+   "<TRAJECTORYPOINTMSG>"
+   "TRAJECTORYPOINTMSG"
+  ))
+
---
+++ devel/share/teb_local_planner/cmake/teb_local_planner-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(teb_local_planner_MESSAGE_FILES "/home/ubuntu/Barn2ROS/src/dependencies/teb_local_planner/msg/ObstacleMsg.msg;/home/ubuntu/Barn2ROS/src/dependencies/teb_local_planner/msg/TrajectoryPointMsg.msg;/home/ubuntu/Barn2ROS/src/dependencies/teb_local_planner/msg/TrajectoryMsg.msg;/home/ubuntu/Barn2ROS/src/dependencies/teb_local_planner/msg/FeedbackMsg.msg")
+set(teb_local_planner_SERVICE_FILES "")
